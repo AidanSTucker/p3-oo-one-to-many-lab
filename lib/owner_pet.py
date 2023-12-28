960374d40989d34f0aee1cd2ab63c5f95937bee3
elpatron <aidanseantucker@gmail.com>
2023-12-28 12:47:05 -0500

    passing 8/9 tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,38 @@
+
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.add_to_all_instances()
+
+    def add_to_all_instances(self):
+        self.__class__.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Not a valid pet")
+
+        pet.owner = self
+        Pet.all.append(pet)
+
+    def get_sorted_pets(self):
+        owner_pets = self.pets()
+        sorted_pets = sorted(owner_pets, key=lambda x: x.name)
+        return sorted_pets
+    
